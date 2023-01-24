dcafa643723e7f7af38dbf70934a43ab591afeb8
Saif Khan <saif.khan@nylas.com@Saifs-MacBook-Pro.local>
2023-01-23 21:01:59 -0800

    Fetch email data from nylas api

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+#Load env variables
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from nylas import APIClient
 
 from sqlalchemy.sql import func
+
+#Initialize Nylas API client
+nylas = APIClient(os.environ.get("CLIENT_ID"),
+    os.environ.get("CLIENT_SECRET"),
+    os.environ.get("ACCESS_TOKEN"),
+)
+#Fetch messages
+messages = nylas.messages
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,8 +34,12 @@ class MyEmail(db.Model):
     date = db.Column(db.Integer)
     label = db.Column(db.String)
 
-
 @app.route('/')
 def index():
+    db.session.query(MyEmail).delete()
+    for message in messages:
+        myemail = MyEmail(id=message.id,sender=message.from_[0]['name'],recipient=message.to[0]['name'],date=message.date,label=message.labels[0]['name'])
+        db.session.add(myemail)
+        db.session.commit()
     myemails = MyEmail.query.all()
     return render_template('index.html', myemails=myemails)
